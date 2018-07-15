aa1b6ecc6fd7d4d9c1fda3b9aedf1412b0b61425
svoruganti <svoruganti@xactlycorp.com>
2018-07-15 12:42:23 +0530

    deleting content in base.java

--- GmailTest/src/test/java/com/xactly/automation/GmailTest/Base.java
+++ GmailTest/src/test/java/com/xactly/automation/GmailTest/Base.java
@@ -13,30 +13,5 @@ import org.testng.annotations.Parameters;
  * Unit test for simple App.
  */
 public class Base {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     * public static String 		RestAssured.baseURI = "http://restapi.demoqa.com/utilities/weather/city";
-     */
-    public Base()
-    {
-        
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+   
 }
